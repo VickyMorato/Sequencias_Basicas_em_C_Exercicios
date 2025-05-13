fdf430bbd80b14158acadb86f4200944292446fe
VickyMorato <vitoriamorato05@gmaial.com>
2025-05-13 14:22:06 -0400

    3 - exercicio reefeito

--- exercicio 3/nome_void_string.c
+++ exercicio 3/nome_void_string.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+int main(void){
     char nomecompleto[2000];
     float salario;
-    int nome;
+    int tamanho_nome;
+    
 
     printf("Escreva seu nome completo:");
     scanf( "%1999[^\n]", nomecompleto);
 
-    nome = strlen(nomecompleto);
+    tamanho_nome = strlen(nomecompleto);
 
     printf("Qual seu salario atual:");
-    scanf("%f", salario);
+    scanf("%f", &salario);
 
     printf("O(a) Funcionario(a) %s tem um salario de %.2f em junho", nomecompleto, salario);
 
